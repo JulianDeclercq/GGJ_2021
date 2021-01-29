823c8b00ba4102f4ebf290e826b5fe5896f5100c
Julian <julian.declercq1@gmail.com>
2021-01-29 23:04:29 +0100

    * Explosion and magnet prototype

---
+++ GGJ_2021/Assets/Scripts/Magnet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    [SerializeField]
+    private float _radius = 5f;
+
+    [SerializeField]
+    private float _forceFactor = 1f;
+
+    private int _layerMask = 0;
+
+    void Start()
+    {
+        _layerMask = ~LayerMask.GetMask("Ignore Everything");
+    }
+
+    void Update()
+    {
+        // TODO: Check if magnetic through layermask
+        var inRadius = Physics.OverlapSphere(transform.position, _radius, _layerMask).Select(x => x.GetComponent<Rigidbody>());
+        foreach(var rb in inRadius)
+            rb.AddForce((transform.position - rb.transform.position) * _forceFactor);
+    }
+    
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+}
