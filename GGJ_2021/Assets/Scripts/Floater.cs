2647bf3a6506cbb93aaf1b6a1e018abe7d34181d
Julian <julian.declercq1@gmail.com>
2021-01-30 15:28:32 +0100

    * Floater

--- GGJ_2021/Assets/Scripts/Floater.cs
+++ GGJ_2021/Assets/Scripts/Floater.cs
@@ -25,43 +25,59 @@ public class Floater : MonoBehaviour
     private List<GameObject> _points;
 
     private Rigidbody _rigidbody;
+    private bool _floats = false;
 
     void Awake()
     {
         Package p = GetComponent<Package>();
-        if (p == null || !p.Properties.HasFlag(PackageProperties.Floats))
-        {
-            Debug.LogError("Package with floater component does not have Floats property");
-            Destroy(this);
-        }
+        _floats = p.Properties.HasFlag(PackageProperties.Floats);
         _rigidbody = GetComponent<Rigidbody>();
 
         // because gravity is calculated manually in the update, it can be disabled for the rigidbody
-        _rigidbody.useGravity = false;
+        if (_floats) 
+            _rigidbody.useGravity = false;
     }
 
     void Update()
     {
         foreach (var point in _points)
         {
-            // gravity calculation
-            _rigidbody.AddForceAtPosition(Physics.gravity / _points.Count, point.transform.position, ForceMode.Acceleration);
+            // gravity calculation, let rigidbody take care of non floaters
+            if (_floats)
+            {
+                _rigidbody.AddForceAtPosition(Physics.gravity / _points.Count, point.transform.position, ForceMode.Acceleration);
+            }
 
-            // if the floater is above the surface, ignore it
+            // if the point is above the surface, ignore it
             if (point.transform.position.y > _surface.transform.position.y)
                 continue;
 
             float displacementMultiplier = Mathf.Clamp01((_surface.transform.position.y - point.transform.position.y) / _depthBeforeSubmerged) * _displacementAmount;
 
-            _rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0), point.transform.position, ForceMode.Acceleration);
-            _rigidbody.AddForce(displacementMultiplier * -_rigidbody.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
-            _rigidbody.AddTorque(displacementMultiplier * -_rigidbody.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            // floating
+            if (_floats)
+            {
+                _rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0), point.transform.position, ForceMode.Acceleration);
+            }
+
+            //_rigidbody.AddForce(displacementMultiplier * -_rigidbody.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            //_rigidbody.AddTorque(displacementMultiplier * -_rigidbody.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            _rigidbody.AddForce(displacementMultiplier * -_rigidbody.velocity * (_waterDrag / _rigidbody.mass) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            _rigidbody.AddTorque(displacementMultiplier * -_rigidbody.angularVelocity * (_waterAngularDrag / _rigidbody.mass) * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
 
-        if(Input.GetKeyDown(KeyCode.L))
+        // manual reset
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            transform.position = new Vector3(transform.position.x, -10, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 2, transform.position.z);
             transform.rotation = Quaternion.Euler(30, transform.rotation.y, transform.rotation.z);
         }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+            transform.rotation = Quaternion.Euler(30, transform.rotation.y, transform.rotation.z);
+        }
+#endif
     }
 }
