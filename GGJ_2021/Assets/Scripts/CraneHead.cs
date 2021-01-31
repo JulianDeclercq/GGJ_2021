99dd576ba0eb1960b82eecedac236c9cfa5ae5c7
Julian <julian.declercq1@gmail.com>
2021-01-31 03:09:24 +0100

    * Tweak scene + physics

--- GGJ_2021/Assets/Scripts/CraneHead.cs
+++ GGJ_2021/Assets/Scripts/CraneHead.cs
@@ -7,31 +7,55 @@ public class CraneHead : MonoBehaviour
     [SerializeField]
     private List<GameObject> _grabbers;
 
+    [SerializeField]
+    private Magnet _magnet;
+
+    [SerializeField]
+    private float _clawSpeed = 1f;
+
+    [SerializeField]
+    private float _angleLimit = 80f;
+
+    [SerializeField]
+    private float _pushingForce = 1f;
+
+    private Rigidbody _rb;
+
     private bool _open = false; //current grabber state
 
-    public float _clawSpeed = 1f;
     private Quaternion _target = Quaternion.identity;
     bool _grabbersBusy = false;
-    
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+            ToggleGrabbers();
+
+        if (Input.GetKeyDown(KeyCode.O))
+            _rb.AddRelativeForce(-Vector3.forward * _pushingForce, ForceMode.Impulse);
+    }
+
     private void ToggleGrabbers()
     {
         if (_grabbersBusy)
             return;
 
         foreach (var grabber in _grabbers)
-            StartCoroutine(RotateGrabber(grabber, _open ? 80f : -80f));
+            StartCoroutine(RotateGrabber(grabber, _open ? _angleLimit : -_angleLimit));
         
         _open = !_open;
-    }
-    void Update()
-    {
-        if (Input.GetButtonUp("Fire1"))
-            ToggleGrabbers();
     }
 
     private IEnumerator RotateGrabber(GameObject grabber, float yDegrees)
     {
         _grabbersBusy = true;
+
+        bool closing = _open;
+        _magnet.enabled = closing;
 
         // Decide the final rotation of the gameobject
         Quaternion target = grabber.transform.localRotation * Quaternion.Euler(new Vector3(yDegrees, 0, 0));
